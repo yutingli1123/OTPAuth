ee7b3e5c9a5ce6c51fa27dce27d309c4ac8164d3
Yuting Li <yutingli1123@outlook.com>
2025-04-18 21:53:42 +0800

    Initialize.

---
+++ src/test/java/fans/goldenglow/otpauth/OtpAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package fans.goldenglow.otpauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtpAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
