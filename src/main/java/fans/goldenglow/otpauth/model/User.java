ca242c9be2e85645f5d070917acebc775813ae59
Yuting Li <yutingli1123@outlook.com>
2025-04-19 09:55:55 +0800

    Add User entity and UserRepository interface.

---
+++ src/main/java/fans/goldenglow/otpauth/model/User.java
@@ -0,0 +1,33 @@
+package fans.goldenglow.otpauth.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true ,nullable = false)
+    private String email;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "last_login")
+    private LocalDateTime lastLogin;
+
+    @Column
+    private boolean active = true;
+}
---
+++ src/main/java/fans/goldenglow/otpauth/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package fans.goldenglow.otpauth.repository;
+
+import fans.goldenglow.otpauth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
